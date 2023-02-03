da428962d98eec0e23c1d32792057a2adfdb8539
Tzvetelin Vassilev <tzvetelin.i.vassilev@gmail.com>
2023-02-03 15:35:18 +0200

    CSSStyleSheet poly-fill implementation

---
+++ src/polyfills/CSSStyleSheet.mjs
@@ -0,0 +1,36 @@
+const CSSStyleSheetOrigin = window.CSSStyleSheet;
+const protoProps = Object.getOwnPropertyNames(CSSStyleSheetOrigin.prototype).slice(1);
+
+class CSSStyleSheet {
+	#style;
+	#sheet;
+
+	constructor() {
+		for (let name of protoProps) {
+			let property = Object.getOwnPropertyDescriptor(CSSStyleSheetOrigin.prototype, name);
+
+			if (typeof property.value == "function")
+				this[name] = property.value.bind(this.#sheet);
+			else {
+				Object.defineProperty(this, name, {
+					get: () => this.#style.sheet[name],
+					configurable: property.configurable,
+					enumerable: property.enumerable
+				});
+			}
+		}
+	}
+
+	replaceSync(text) {
+		let style = document.createElement("style");
+		style.innerHTML = text;
+
+		document.head.appendChild(style);
+
+		this.#style = style;
+		this.#sheet = style.sheet;
+	}
+}
+
+if (!CSSStyleSheetOrigin.prototype.replaceSync)
+	window.CSSStyleSheet = CSSStyleSheet;
