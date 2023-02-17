87e12807461af988e4dcbbd67276466a75c98ec0
Tzvetelin Vassilev <tzvetelin.i.vassilev@gmail.com>
2023-02-17 13:00:11 +0200

    createClass improved

--- src/classes/extensions/FunctionExt.mjs
+++ src/classes/extensions/FunctionExt.mjs
@@ -23,17 +23,35 @@ class FunctionExt extends Extension {
 	 * Create class with name as argument
 	 *
 	 * @param {string} name Class name
-	 * @param {Class} [parentClass] When is available, class definition extends this class
+	 * @param {Class} parentClass Class definition extends this class
+	 * @param {JSON} [options={}] Class semantic
+	 * @example <caption>Options sample</caption>
+	 * {
+	 *   constructorSrc: `
+	 *     this.#init();
+	 *   `,
+	 *   methods: `
+	 *     #init() {
+	 *       let options = helper.getConfiguration();
+	 *       // set custom configuration
+	 *     }
+	 *   `,
+	 *   args: {helper: MyHelper}
+	 * }
 	 * @returns {Class} Class definition
 	 */
-	static createClass(name, parentClass) {
-		const def = new Function(parentClass ? parentClass.name : undefined, `return class ${name}${parentClass ? ` extends ${parentClass.name}` : ""} {
+	createClass(name, parentClass, options = {}) {
+		let args = options.args || {};
+
+		const def = new Function(parentClass.name, ...Object.keys(args), `return class ${name} extends ${parentClass.name} {
 			constructor() {
-				${parentClass ? "super(...arguments);" : ""}
+				super(...arguments);
+				${options.constructorSrc || ""}
 			}
+			${options.methods || ""}
 		}`);
 
-		return def(parentClass);
+		return def(parentClass, ...Object.values(args));
 	}
 }
 
